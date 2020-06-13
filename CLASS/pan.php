6637d9fb2e0b7b6ffbbd1193799f7c99d6e6cfbe
hiepho992 <dinhhiep211992@gmail.com>
2020-06-13 09:50:22 +0700

    php

--- CLASS/pan.php
+++ CLASS/pan.php
@@ -71,6 +71,12 @@ $fan1->setRadius(10);
 $fan1->setColor("yellow");
 $fan1->setOn(true);
 echo $fan1->toString();
+$fan2 = new Fan();
+$fan2->setSpeed($fan1::MEDIUM);
+$fan2->setRadius(5);
+$fan2->setColor("blue");
+$fan2->setOn(false);
+echo $fan2->toString();
 
 
 
